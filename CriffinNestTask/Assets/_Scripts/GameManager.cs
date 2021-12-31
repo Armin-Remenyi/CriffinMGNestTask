26401b93d4bb22ce71e6adaee012e3e42c0bbb7c
Armin Remenyi <remenyiarminlaszlo@gmail.com>
2021-12-31 02:51:28 +0200

    restart fixes
    minor fixes on the restart button (still needs work)

---
+++ CriffinNestTask/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject GameOver, Restart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        GameOver.gameObject.SetActive(false);
+        Restart.gameObject.SetActive(false);
+    }
+}
--- CriffinNestTask/Assets/_Scripts/Restart.cs
+++ CriffinNestTask/Assets/_Scripts/Restart.cs
@@ -2,12 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Restart : MonoBehaviour
 {
+    void Start()
+    {
+        UnlockMouse();
+    }
+    
+    void UnlockMouse()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
     public void RestartGame()
     {
         Debug.Log("Restart");
+        //SceneManager.LoadScene("MachineGunNest");
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
